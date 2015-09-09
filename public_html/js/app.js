0f55d95aa45388178e5ec529ba39054d7f16fcf2
Jon Forsyth <jon4syth@gmail.com>
2015-09-09 10:17:02 -0600

    start blog project

---
+++ public_html/js/app.js
@@ -0,0 +1,25 @@
+
+/* chapter5/app.js */
+
+'use strict'
+/* App Module */
+
+var blogApp = angular.module('blogApp', [
+	'ngRoute',
+	'blogControllers'
+]);
+
+blogApp.config(['$routeProvider', '$locationProvider',
+  function($routeProvider, $locationProvider){
+
+	  $routeProvider.
+	      when('/', {
+		templateUrl: 'partials/main.html',
+		controller: 'BlogCtrl'
+	  }).when('/blogPost/:id', {
+		 templateUrl: 'partials/blogPost.html',
+		 controller: 'BlogViewCtrl'
+	  });
+
+	$locationProvider.html5Mode(false).hashPrefix('!');
+  }]);
